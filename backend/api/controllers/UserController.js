5ffdb268288e914cf9338d1978a1186565922277
Danjavia <danjavia.iconshock@gmail.com>
2016-01-17 10:39:37 -0500

    rigistrationi process

--- backend/api/controllers/UserController.js
+++ backend/api/controllers/UserController.js
@@ -34,6 +34,7 @@ module.exports = {
 		var username = req.param( "username" );
         var password = req.param( "password" );
         var avatar = req.param( "avatar" );
+        var validate = req.param( "validate" );
 
         User.findByUsername( username ).exec( function( err, usr ) {
             if ( err ) {
@@ -41,14 +42,44 @@ module.exports = {
             } else if ( usr.length > 0 ) {
                 res.json({ code: 400, error: "Username already Taken" });
             } else {
-                User.create({ username: username, password: password, avatar: avatar }).exec( function( error, user ) {
-	                if ( error ) {
-	                    res.json({ code: 500, error: "DB Error" });
-	                } else {
-	                    req.session.user = user;
-	                    res.json( user );
-	                }
-	            });
+
+            	// Validate user another again in the second step
+            	if ( validate ) {
+
+            		// User variables from billing info
+            		var name = req.param( "name" )
+            		var	ccn = req.param( "ccn" )
+            		var	ccv = req.param( "ccv" )
+            		var	ccd = req.param( "ccd" )
+
+            		// object from billingInfo form
+            		var	billing_info = {
+            				name: name,
+            				ccn: ccn,
+            				ccv: ccv,
+            				ccd: ccd
+            			}
+
+            		// object from simple login form
+            		var userData = {
+            			username: username, 
+            			password: password, 
+            			avatar: avatar, 
+            			billing_info: billing_info
+            		}
+
+            		// Create user
+	                User.create( userData ).exec( function( error, user ) {
+		                if ( error ) {
+		                    res.json({ code: 500, error: "DB Error" });
+		                } else {
+		                    req.session.user = user;
+		                    res.json( user );
+		                }
+		            });
+            	}
+	            else
+	            	res.json({ proceed: true })
 	        };
     	});
 	}
